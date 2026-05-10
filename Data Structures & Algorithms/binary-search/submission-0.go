bf71b892e17339718fcad18e8d12bfc06022bdb6
Avinash Kemparaju <kemparaju.avinash@gmail.com>
2026-05-10 19:14:21 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.go
@@ -0,0 +1,16 @@
+func search(nums []int, target int) int {
+	i, j := 0, len(nums)-1
+	for i <= j {
+		mid := int((i+j)/2)
+		
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] < target {
+			i = mid + 1
+		} else {
+			j = mid - 1
+		}
+	}
+	return -1
+}
