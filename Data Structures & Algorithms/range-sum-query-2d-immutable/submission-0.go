5b09a15cc19708d76c1b4ccd453c7b5532c123ac
Avinash Kemparaju <kemparaju.avinash@gmail.com>
2026-06-04 08:02:07 +0530

    Add: range-sum-query-2d-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-0.go
@@ -0,0 +1,24 @@
+
+type NumMatrix struct {
+	nums [][]int
+}
+
+func Constructor(matrix [][]int) NumMatrix {
+	n := NumMatrix{matrix}
+	fmt.Println(n)
+	return n
+}
+
+func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
+	sum := 0
+	for i := row1; i <= row2; i++ {
+		for j := col1; j <= col2; j++ {
+			sum += this.nums[i][j]
+		}
+	}
+	return sum
+}
+
+// Your NumMatrix object will be instantiated and called as such:
+// obj := Constructor(matrix)
+// param_1 := obj.SumRegion(row1,col1,row2,col2)
