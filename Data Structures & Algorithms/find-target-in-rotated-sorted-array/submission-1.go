dd6036b29ee08875e3355cef374031702608f2bc
Avinash Kemparaju <kemparaju.avinash@gmail.com>
2026-07-04 23:02:07 +0530

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.go
@@ -0,0 +1,28 @@
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l+r)/2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[l] <= nums[mid] {
+			// left sorted portion
+			if target > nums[mid] || target < nums[l] {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		} else {
+			// right sorted portion
+			if target < nums[mid] || target > nums[r] {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		}
+
+	}
+
+	return -1
+}
