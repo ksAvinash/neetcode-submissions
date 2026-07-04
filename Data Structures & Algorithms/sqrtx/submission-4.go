c9b59ae4f58e3eece55926e95c12580400b8a660
Avinash Kemparaju <kemparaju.avinash@gmail.com>
2026-07-04 09:15:21 +0530

    Add: sqrtx - submission-4

---
+++ Data Structures & Algorithms/sqrtx/submission-4.go
@@ -0,0 +1,33 @@
+func mySqrt(x int) int {
+	// brute force
+	// if x < 2 {
+	// 	return x
+	// }
+	// val := 0
+	// for i := range x {
+	// 	if i * i <= x {
+	// 		val = i
+	// 	} else {
+	// 		break
+	// 	}
+	// }
+	// return val
+
+
+	if x < 2 {
+		return x
+	}
+	left, right := 1, x
+	val := 1
+	for left <= right {
+		mid := (left+right)/2
+
+		if mid * mid <= x {
+			val = mid
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return val
+}
