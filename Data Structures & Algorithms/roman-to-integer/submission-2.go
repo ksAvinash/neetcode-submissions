85dbe2eb3b649ddee0481142f7fe885ca25c3f37
Avinash Kemparaju <kemparaju.avinash@gmail.com>
2026-07-11 13:20:17 +0530

    Add: roman-to-integer - submission-2

---
+++ Data Structures & Algorithms/roman-to-integer/submission-2.go
@@ -0,0 +1,21 @@
+
+
+func romanToInt(s string) int {
+	count, i := 0, 0
+	values := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+	for i < len(s) {
+		v, n := values[string(s[i])], 0
+		if i < len(s)-1 {
+			n = values[string(s[i+1])]
+		}
+		if v < n {
+			count += n - v
+			i++
+		} else {
+			count += v
+		}
+		i++
+	}
+	return count
+}
