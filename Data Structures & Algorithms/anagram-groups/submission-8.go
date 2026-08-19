b0a3ef6df2d01ea73418f5194b79b693ef3268dd
Avinash Kemparaju <kemparaju.avinash@gmail.com>
2026-08-19 18:52:33 +0530

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.go
@@ -0,0 +1,22 @@
+import "slices"
+
+func sortString(s string) string {
+	runes := []rune(s)
+	slices.Sort(runes)
+	return string(runes)
+}
+
+func groupAnagrams(strs []string) [][]string {
+	anagrams := make(map[string][]string)
+
+	for _, s := range strs {
+		key := sortString(s)
+		anagrams[key] = append(anagrams[key], s)
+	}
+
+	res := make([][]string, 0, len(anagrams))
+	for _, v := range anagrams {
+		res = append(res, v)
+	}
+	return res
+}
