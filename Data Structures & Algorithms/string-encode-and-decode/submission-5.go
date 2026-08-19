9847321be0b011e03d9c0b8af657d94699810c5c
Avinash Kemparaju <kemparaju.avinash@gmail.com>
2026-08-19 20:20:00 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.go
@@ -0,0 +1,27 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+	res := ""
+	for _, s := range strs {
+		// <len> # <str>
+		// res += fmt.Sprintf("%d", len(s)) + "#" + s
+		res += strconv.Itoa(len(s)) + "#" + s
+	}
+	return res
+}
+
+func (s *Solution) Decode(encoded string) []string {
+	i, res := 0, []string{}
+
+	for i < len(encoded) {
+		j := i
+		for encoded[j] != '#' {
+			j++
+		}
+		slen, _ := strconv.Atoi(encoded[i:j])
+		res = append(res, encoded[j+1 : j+1+slen])
+		i = j+1+slen
+	}
+
+	return res
+}
