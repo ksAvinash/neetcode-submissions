18872ec307177cc632cf1b10489e22ca34c89bc2
Avinash Kemparaju <kemparaju.avinash@gmail.com>
2026-08-30 18:36:13 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.go
@@ -0,0 +1,36 @@
+type MinStack struct {
+	items []int
+	minItem int
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		items: []int{},
+		minItem: math.MaxInt,
+	}
+}
+
+func (this *MinStack) Push(val int) {
+	this.items = append(this.items, val)
+	this.minItem = min(this.minItem, val)
+}
+
+func (this *MinStack) Pop() {
+	top := this.Top()
+	this.items = this.items[:len(this.items)-1]
+
+	if top == this.minItem {
+		this.minItem = math.MaxInt
+		for _, v := range this.items {
+			this.minItem = min(this.minItem, v)
+		}
+	}
+}
+
+func (this *MinStack) Top() int {
+	return this.items[len(this.items)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minItem
+}
